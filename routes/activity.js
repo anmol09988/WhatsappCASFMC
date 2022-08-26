706c42afe3d69e8679621f5e1dbb2ec5ae8df878
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-08-26 15:43:23 +0530

    saved

--- routes/activity.js
+++ routes/activity.js
@@ -47,11 +47,17 @@ exports.save = function (req, res) {
 
 exports.execute = function (req, res) {
 
-    // const requestBody = req.body.inArguments[0];
+    const requestBody = req.body.inArguments[0];
 
-    // const SubscriberKey = requestBody.SubscriberKey;
-    // const EmailAddress = requestBody.EmailAddress;
-    // const eventDefinationKey = requestBody.body;
+    const SubscriberKey = requestBody.SubscriberKey;
+    const EmailAddress = requestBody.EmailAddress;
+    const Time = requestBody.EmailAddress;
+    const eventDefinationKey = requestBody.body;
+
+    console.log("SubscriberKeySubscriberKey"+SubscriberKey);
+    console.log("EmailAddressEmailAddress"+EmailAddress);
+    console.log("TimeTime"+Time);
+    console.log("eventDefinationKeyeventDefinationKey"+eventDefinationKey);
 
     // var accessTokenRequest = require('request');
     // var accessTokenGetBody = {
@@ -118,39 +124,39 @@ exports.validate = function (req, res) {
     console.log("5");
     console.log("invalidatefunction");
 
-    var journyTrigger = require('request');
-    var authorizationtoken = 'EAAFoGCDT8BIBAGl6No1V1MWnHZC19YKAKZC9b7uCg5SxlZClFd7MdZAmZBLT42oy4ZBGhL2BCyMLH7GeZBedb2zfFkakDk1aq1yd0fQ7yO3ZASQbFiOK2hoEUqZCGjQVhki31dO3ZA9FsZC58Sozh87p047YAhlZA0CbaqeN4LA4RqTUU1FXatF9fvTisLDK8h0AZCHZAMNLn4ZAXeDrms5iWZAT5Bui';
-    var bearerToken = 'Bearer ' + authorizationtoken;
-    //  var eventKey = eventDefinationKey;
+    // var journyTrigger = require('request');
+    // var authorizationtoken = 'EAAFoGCDT8BIBAGl6No1V1MWnHZC19YKAKZC9b7uCg5SxlZClFd7MdZAmZBLT42oy4ZBGhL2BCyMLH7GeZBedb2zfFkakDk1aq1yd0fQ7yO3ZASQbFiOK2hoEUqZCGjQVhki31dO3ZA9FsZC58Sozh87p047YAhlZA0CbaqeN4LA4RqTUU1FXatF9fvTisLDK8h0AZCHZAMNLn4ZAXeDrms5iWZAT5Bui';
+    // var bearerToken = 'Bearer ' + authorizationtoken;
+    // //  var eventKey = eventDefinationKey;
 
-    var journeyBody = {
-         "messaging_product": "whatsapp",
-        "to": "918949782691",
-        "type": "template",
-        "template": {
-            "name": "hello_world",
-            "language": {
-                "code": "en_US"
-            }
-        }
-    }
+    // var journeyBody = {
+    //      "messaging_product": "whatsapp",
+    //     "to": "918949782691",
+    //     "type": "template",
+    //     "template": {
+    //         "name": "hello_world",
+    //         "language": {
+    //             "code": "en_US"
+    //         }
+    //     }
+    // }
 
-    journyTrigger({
-        headers: {
-            'Authorization': bearerToken,
-            'Content-Type': 'application/json'
-        },
-        url: "https://graph.facebook.com/v13.0/107909608687000/messages",
-        method: "POST",
-        json: true,
-        body: journeyBody
-    }, function (error, res, body) {
-        statusCode = res.statusCode;
-        var authorizationtokensss = JSON.stringify(res.body);
-        console.log('statusCode'+statusCode);
-        console.log('authorizationtokensss'+authorizationtokensss);
+    // journyTrigger({
+    //     headers: {
+    //         'Authorization': bearerToken,
+    //         'Content-Type': 'application/json'
+    //     },
+    //     url: "https://graph.facebook.com/v13.0/107909608687000/messages",
+    //     method: "POST",
+    //     json: true,
+    //     body: journeyBody
+    // }, function (error, res, body) {
+    //     statusCode = res.statusCode;
+    //     var authorizationtokensss = JSON.stringify(res.body);
+    //     console.log('statusCode'+statusCode);
+    //     console.log('authorizationtokensss'+authorizationtokensss);
 
-    });
+    // });
 
     logData(req);
     res.send(200, 'Validate');
