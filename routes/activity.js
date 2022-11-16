537f9e653868d05f7263432dc8c32700b7012448
anmol09988 <107464613+anmol09988@users.noreply.github.com>
2022-11-16 16:51:35 +0530

    updated

--- routes/activity.js
+++ routes/activity.js
@@ -55,63 +55,6 @@ exports.save = function (req, res) {
 exports.execute = function (req, res) {
 
 
-    // const requestBody = req.body.inArguments[0];
-
-    // const SubscriberKey = requestBody.SubscriberKey;
-    // const EmailAddress = requestBody.EmailAddress;
-    // const eventDefinationKey = requestBody.body;
-
-    // var accessTokenRequest = require('request');
-    // var accessTokenGetBody = {
-    //     "grant_type": "client_credentials",
-    //     "client_id": "ewozgxquu4nriupcx2tylyfl",
-    //     "client_secret": "d3BNHjIK6RAZQi7VgbXVYnWw",
-    //     "account_id": "526000739"
-    // };
-    // accessTokenRequest({
-    //     url: "https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.auth.marketingcloudapis.com/v2/token",
-    //     method: "POST",
-    //     json: true,
-    //     body: accessTokenGetBody
-    // }, function (error, response, body) {
-    //     statusCode = response.statusCode;
-
-    //     if (statusCode === 200) {
-    //         var authorizationtoken = JSON.parse(JSON.stringify(response.body))['access_token'];
-    //         journeyTrigger(authorizationtoken, eventDefinationKey);
-    //     }
-    // });
-
-    // function journeyTrigger(authorizationtoken, eventDefinationKey) {
-
-    //     var journyTrigger = require('request');
-    //     var bearerToken = 'Bearer ' + authorizationtoken;
-    //     var eventKey = eventDefinationKey;
-
-    //     var journeyBody = {
-    //         "definitionKey": eventKey,
-    //         "recipients":
-    //             [
-    //                 {
-    //                     "contactKey": SubscriberKey,
-    //                     "to": EmailAddress
-    //                 }
-    //             ]
-    //     };
-
-    //     journyTrigger({
-    //         headers: {
-    //             'Authorization': bearerToken,
-    //             'Content-Type': 'application/json'
-    //         },
-    //         url: "https://mch4s3mv5j6r7tyf5xqf8s0-y2wm.rest.marketingcloudapis.com/messaging/v1/email/messages",
-    //         method: "POST",
-    //         json: true,
-    //         body: journeyBody
-    //     }, function (error, res, body) {
-
-    //     });
-    // };
 
     res.send(200, 'Execute');
 };
@@ -128,7 +71,7 @@ exports.validate = function (req, res) {
 
     var journyTrigger = require('request');
     var bearerToken = 'Bearer ' + authorizationtoken;
-    console.log('bearerToken'+bearerToken);
+    console.log('bearerToken' + bearerToken);
 
     var journeyBody = {
         "messaging_product": "whatsapp",
@@ -144,20 +87,18 @@ exports.validate = function (req, res) {
 
     journyTrigger({
         headers: {
-            'Authorization': bearerToken,
-            'Content-Type': 'application/json'
+            'Authorization : ': bearerToken,
+            'Content-Type : ': 'application/json'
         },
-        url: " https://graph.facebook.com/v15.0/107909608687000/messages",
+        url: "https://graph.facebook.com/v15.0/107909608687000/messages",
         method: "POST",
         json: true,
         body: journeyBody
     }, function (error, res, body) {
-        // statusCode = res;
         var auth = JSON.parse(JSON.stringify(res.body))
         console.log(auth);
-
     });
 
     logData(req);
     res.send(200, 'Validate');
-};       
+};
